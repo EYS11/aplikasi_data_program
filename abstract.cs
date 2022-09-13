79f6bab7fc63d78de326e4c698eaa4b80ba111ef
Ester Yuliana Yagala <99631801+EYS11@users.noreply.github.com>
2022-09-13 20:27:15 +0700

    Add files via upload

---
+++ abstract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic; //untuk menampilkan list
+
+namespace Aplikasi_Data_Pegawai
+{
+    //Abstraction
+
+    public abstract class Abstract
+    {
+        protected static List<string> pegawai = new List<string>();
+        protected static List<string> nik = new List<string>();
+        protected static List<int> masakerja = new List<int>();
+        protected static List<string> jabatan = new List<string>();
+        protected static List<int> gajipokok = new List<int>();
+        protected static List<int> gajibulanan = new List<int>();
+        
+
+    }
+}
